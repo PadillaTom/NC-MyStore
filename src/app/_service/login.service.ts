356fe78976a547023abb8d0199e279ebc7f2162d
Tomas Padilla <padillatomasagustin@gmail.com>
2022-02-25 16:59:56 +0100

    push

--- src/app/_service/login.service.ts
+++ src/app/_service/login.service.ts
@@ -3,36 +3,29 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment';
 
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LoginService {
-  private url: string = `${environment.HOST}/auth/login`;
+  private url: string = `http://localhost:8080/auth/login`;
   //private url: string = `${environment.HOST}/oauth/token`;
 
-  constructor(
-    private http: HttpClient,
-    private router: Router
-  ) { }
+  constructor(private http: HttpClient, private router: Router) {}
 
   login(email: string, password: string) {
-    const body = `grant_type=password&email=${encodeURIComponent(email)}&password=${encodeURIComponent(password)}`;
+    const body = `grant_type=password&email=${encodeURIComponent(
+      email
+    )}&password=${encodeURIComponent(password)}`;
 
     return this.http.post(this.url, body, {
       headers: new HttpHeaders()
-      .set('contentType', 'application/x-www-form-urlencoded; charset=utf-8')
-      .set('Authorization', 'Basic ' 
-      + btoa(environment.TOKEN_AUTH_EMAIL + ':' + environment.TOKEN_AUTH_PASSWORD))
+        .set('contentType', 'application/x-www-form-urlencoded; charset=utf-8')
+        .set('Authorization', 'Basic ' + btoa('ADMIN@test.com:tienda1234')),
     });
-
   }
 
   cerrarSesion() {
     sessionStorage.clear();
     this.router.navigate(['login']);
   }
-
-
-
 }
