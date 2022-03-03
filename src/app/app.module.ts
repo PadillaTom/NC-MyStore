be0646e6627d3860a4a9c0f3500b684549bc6f9f
Tomas Padilla <padillatomasagustin@gmail.com>
2022-03-03 09:28:32 +0100

    Deploy - Login, Register, CategoriesActive, Products, DetailedProduct

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-
 import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
@@ -15,12 +14,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatCardModule } from '@angular/material/card';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
-import { environment } from 'src/environments/environment';
 import { HttpClientModule } from '@angular/common/http';
-
-export function tokenGetter() {
-  return sessionStorage.getItem(environment.TOKEN_NAME);
-}
 
 @NgModule({
   declarations: [AppComponent],
